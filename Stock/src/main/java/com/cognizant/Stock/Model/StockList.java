0163d3b662225b866e0133772241b4bad7a8fdbe
Pratyush Vatsal Shukla <54927584+pratyushvatsalshukla@users.noreply.github.com>
2024-02-04 01:12:19 +0530

    Added Eureka, Stock User Service

---
+++ Stock/src/main/java/com/cognizant/Stock/Model/StockList.java
@@ -0,0 +1,32 @@
+package com.cognizant.Stock.Model;
+
+public class StockList {
+	private Stock[] data;
+	private String status;
+	
+	public StockList()
+	{
+		super();
+	}
+	
+	public StockList(Stock[] data, String status) {
+		this.data=data;
+		this.status=status;
+	}
+	
+	public Stock[] getData() {
+		return data;
+	}
+	public void setData(Stock[] data) {
+		this.data=data;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status)
+	{
+		this.status=status;
+	}
+}
