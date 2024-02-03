352477b4b89d5ecd641a0dcbca62c4ef756a44f7
Pratyush Vatsal Shukla <54927584+pratyushvatsalshukla@users.noreply.github.com>
2024-02-04 01:05:59 +0530

    Backend (#1)
    * Delete Backend directory
    
    * Delete frontend directory

--- frontend/src/auth/Islogin.js
+++
@@ -1,9 +0,0 @@
-export const Islogin = () => {
-    let data = localStorage.getItem("token");
-    if(data != null) {
-        return true;
-    }
-
-    else {return false;
-}
-};
--- frontend/src/auth/Logout.js
+++
@@ -1,6 +0,0 @@
-
-export const Logout=(next) => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("email");
-    next()
-};
